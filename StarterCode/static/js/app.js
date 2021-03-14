bd31d9679664e2fac68172c93fca4d78e05d82b6
Ryan EM <ryan.eccleston.murdock@gmail.com>
2021-03-13 22:00:46 -0500

    finalizing

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -8,7 +8,6 @@ function populateDropdown(data) {
     };
 };
 
-
 d3.json('samples.json').then(data => {
     // make h bar chart for top 1o otu_ids for a given individual 
     var data_samples = data.samples;
@@ -26,6 +25,7 @@ d3.json('samples.json').then(data => {
         type: 'bar',
         x: lookupable_data[940].sample_values.slice(0,10),
         y: lookupable_data[940].otu_ids.map(pt => 'OTU ' + pt).slice(0,10),
+        text: lookupable_data[940].otu_labels.slice(0,10),
         orientation: 'h'
     }
     var data_to_plot = [trace_bar];
@@ -93,9 +93,3 @@ d3.json('samples.json').then(data => {
 
     dropdown.on('change', handleTestSubjectData);
 });
-
-
-// input data to #selDataset
-// listen for change on test subject ID 
-//     send value to makeTrace
-// make h bar from trace 
