b9a1f57c260d4a8965c608e0983ea1bee4470f39
Ryan EM <ryan.eccleston.murdock@gmail.com>
2021-03-13 21:19:28 -0500

    adding bubble chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -18,30 +18,64 @@ d3.json('samples.json').then(data => {
     data.forEach(pt => lookupable_data[pt.id] = pt);
 
     // make default bar chart
-    var trace = {
+    var trace_bar = {
         type: 'bar',
         x: lookupable_data[940].sample_values.slice(0,10),
         y: lookupable_data[940].otu_ids.map(pt => 'OTU ' + pt).slice(0,10),
         orientation: 'h'
     }
-    var data_to_plot = [trace];
+    var data_to_plot = [trace_bar];
     Plotly.newPlot('bar', data_to_plot);
+
+    //make default bubble chart 
+    var trace_bubble = {
+        x: lookupable_data[940].otu_ids,
+        y: lookupable_data[940].sample_values,
+        text: lookupable_data[940].otu_labels,
+        mode: 'markers',
+        marker: {
+          color: lookupable_data[940].otu_ids,
+          opacity: 0.4,
+          size: lookupable_data[940].sample_values
+        }
+      };
+
+      var data_to_plot = [trace_bubble];
+
+      Plotly.newPlot('bubble', data_to_plot);
 
     // handling different subjects
     function handleTestSubjectData() {
-        // d3.event.preventDefault();
         var subject_id = d3.select(this).node().value;
         var relevant_subject = lookupable_data[subject_id];
 
-        var trace = {
+        // h bar
+        var trace_bar = {
             type: 'bar',
             x: relevant_subject.sample_values.slice(0,10),
             y: relevant_subject.otu_ids.map(pt => 'OTU' + pt).slice(0,10),
             orientation: 'h'
         }
 
-        var data_to_plot = [trace];
+        var data_to_plot = [trace_bar];
         Plotly.newPlot('bar', data_to_plot);
+
+        // bubble
+        var trace_bubble = {
+            x: relevant_subject.otu_ids,
+            y: relevant_subject.sample_values,
+            text: relevant_subject.otu_labels,
+            mode: 'markers',
+            marker: {
+              color: relevant_subject.otu_ids,
+              opacity: 0.6,
+              size: relevant_subject.sample_values
+            }
+          };
+    
+          var data_to_plot = [trace_bubble];
+    
+          Plotly.newPlot('bubble', data_to_plot);
     };
 
     dropdown.on('change', handleTestSubjectData);
