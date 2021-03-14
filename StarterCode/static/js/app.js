e2dc37a1fbfdcf91cfcee08f0940b168ff622b4b
Ryan EM <ryan.eccleston.murdock@gmail.com>
2021-03-13 21:56:40 -0500

    added demographics card per subject

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -11,11 +11,15 @@ function populateDropdown(data) {
 
 d3.json('samples.json').then(data => {
     // make h bar chart for top 1o otu_ids for a given individual 
-    var data = data.samples;
-    populateDropdown(data);
+    var data_samples = data.samples;
+    var meta_data = data.metadata;
+    populateDropdown(data_samples);
     
     var lookupable_data = {};
-    data.forEach(pt => lookupable_data[pt.id] = pt);
+    data_samples.forEach(pt => lookupable_data[pt.id] = pt);
+
+    var lookupable_metadata = {};
+    meta_data.forEach(pt => lookupable_metadata[pt.id] = pt);
 
     // make default bar chart
     var trace_bar = {
@@ -27,7 +31,7 @@ d3.json('samples.json').then(data => {
     var data_to_plot = [trace_bar];
     Plotly.newPlot('bar', data_to_plot);
 
-    //make default bubble chart 
+    // make default bubble chart 
     var trace_bubble = {
         x: lookupable_data[940].otu_ids,
         y: lookupable_data[940].sample_values,
@@ -39,24 +43,31 @@ d3.json('samples.json').then(data => {
           size: lookupable_data[940].sample_values
         }
       };
-
       var data_to_plot = [trace_bubble];
-
       Plotly.newPlot('bubble', data_to_plot);
+    
+    // populate defaut demographics 
+    var demographic_info = d3.select('#sample-metadata');
+    var demo_keys = Object.keys(lookupable_metadata[940]);
+    demo_keys.forEach(key => demographic_info.append('p').text(`${key}: ${lookupable_metadata[940][key]}`));
+    console.log(demographic_info);
+    console.log(lookupable_metadata);
+    
 
     // handling different subjects
     function handleTestSubjectData() {
         var subject_id = d3.select(this).node().value;
         var relevant_subject = lookupable_data[subject_id];
 
+        console.log(relevant_subject.otu_labels);
         // h bar
         var trace_bar = {
             type: 'bar',
             x: relevant_subject.sample_values.slice(0,10),
             y: relevant_subject.otu_ids.map(pt => 'OTU' + pt).slice(0,10),
+            text: relevant_subject.otu_labels.slice(0,10),
             orientation: 'h'
         }
-
         var data_to_plot = [trace_bar];
         Plotly.newPlot('bar', data_to_plot);
 
@@ -72,14 +83,15 @@ d3.json('samples.json').then(data => {
               size: relevant_subject.sample_values
             }
           };
-    
           var data_to_plot = [trace_bubble];
-    
           Plotly.newPlot('bubble', data_to_plot);
+
+        // demographics card
+        demographic_info.html('');
+        demo_keys.forEach(key => demographic_info.append('p').text(`${key}: ${lookupable_metadata[subject_id][key]}`));
     };
 
     dropdown.on('change', handleTestSubjectData);
-
 });
 
 
